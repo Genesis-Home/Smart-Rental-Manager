ea7da50580fddfde6a1130dfa5b56fb248e5b416
AneelaShakeel1 <eb20103017.aneelashakeel@gmail.com>
2025-05-16 17:58:50 +0500

    email send at booking create [ADD]

--- src/store/actions/action.ts
+++ src/store/actions/action.ts
@@ -6,76 +6,85 @@ import { deleteItem, setItem } from "../../services/assynsStorage";
 import Toast from "react-native-toast-message";
 import getFirebaseErrorMessage from "../../services/firebaseErrorHandler";
 import { scheduleBookingNotifications } from "../../services/notificationService";
-import { sendEmailAPI } from "../../services/emailService";
 import axios from "axios";
 
-export const getCurrentUser = (navigation: NavigationProp<any>): any => async (dispatch: Dispatch) => {
-  // Add your implementation here
-};
+export const getCurrentUser =
+  (navigation: NavigationProp<any>): any =>
+  async (dispatch: Dispatch) => {
+    // Add your implementation here
+  };
 
-export const sendEmail = (navigation: NavigationProp<any>): any => async (dispatch: Dispatch) => {
-  try {
-    const token = await auth().currentUser.getIdToken();
-    const response = await axios.post(
-      'https://api-youshwrkza-uc.a.run.app/send-email',
-      {
-        to: 'abddullahshah@gmail.com',
-        subject: 'Test email',
-        message: 'This is a test email.',
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
+export const sendEmail =
+  (
+    navigation: NavigationProp<any>,
+    email: string,
+    visitDates: string,
+    visitTime: string,
+    numberOfVisitors: string,
+    numberOfInfants: string,
+    property: string
+  ): any =>
+  async (dispatch: Dispatch) => {
+    try {
+      const formattedVisitDates = visitDates.replace(" - ", " to ");
+      const response = await axios.post(
+        "https://api-youshwrkza-uc.a.run.app/send-email",
+        {
+          to: email,
+          subject: "Your visit is confirmed",
+          message: `Visit Details\nVisit Date & Time: ${formattedVisitDates}, ${visitTime}\nNumber of Visitors: ${numberOfVisitors}\nNumber of Infants: ${numberOfInfants}\nProperty Address: ${property}`,
         },
-      }
-    );
-    console.log('Email sent successfully:', response.data);
-  } catch (error) {
-    console.error('Error sending email:', error);
-  }
-};
-
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("Email sent successfully:", response.data);
+      navigation.navigate("AutomatedEmail");
+    } catch (error) {
+      console.error("Error sending email:", error);
+    }
+  };
 
 export const loginUser =
   (credentials: any, isSelectedRemember: any, navigation: any) =>
-    async (dispatch: any) => {
-      try {
-        dispatch({ type: "IS_LOADER", payload: true });
-        // User Login
-        const userCredential = await auth().signInWithEmailAndPassword(
-          credentials.email,
-          credentials.password
-        );
-        console.log("user", userCredential);
+  async (dispatch: any) => {
+    try {
+      dispatch({ type: "IS_LOADER", payload: true });
+      // User Login
+      const userCredential = await auth().signInWithEmailAndPassword(
+        credentials.email,
+        credentials.password
+      );
+      console.log("user", userCredential);
 
-        const user = (userCredential.user as any)._user;
+      const user = (userCredential.user as any)._user;
 
-        const userDoc = await firestore().collection("users").doc(user.uid).get();
+      const userDoc = await firestore().collection("users").doc(user.uid).get();
 
-        const userData = userDoc.data();
+      const userData = userDoc.data();
 
-        isSelectedRemember && setItem("user", userData);
+      isSelectedRemember && setItem("user", userData);
 
-        !isSelectedRemember && deleteItem("user");
+      !isSelectedRemember && deleteItem("user");
 
-        dispatch({ type: "SET_USER", payload: userData });
-        dispatch({ type: "IS_LOADER", payload: false });
+      dispatch({ type: "SET_USER", payload: userData });
+      dispatch({ type: "IS_LOADER", payload: false });
 
-        navigation.dispatch(
-          CommonActions.reset({ index: 0, routes: [{ name: "Tabs" }] })
-        );
-        const customMessage = await getFirebaseErrorMessage("Login successful!");
-        Toast.show({ type: "success", text1: customMessage, position: "bottom" });
-        Toast.show({ type: "success", text1: customMessage, position: "bottom" });
-        dispatch(sendEmail(navigation));
-
-      } catch (error) {
-        console.log(error, "loginUser_error");
-        dispatch({ type: "IS_LOADER", payload: false });
-        const errorMessage = await getFirebaseErrorMessage((error as any).code);
-        Toast.show({ type: "error", text1: errorMessage, position: "bottom" });
-      }
-    };
+      navigation.dispatch(
+        CommonActions.reset({ index: 0, routes: [{ name: "Tabs" }] })
+      );
+      const customMessage = await getFirebaseErrorMessage("Login successful!");
+      Toast.show({ type: "success", text1: customMessage, position: "bottom" });
+      Toast.show({ type: "success", text1: customMessage, position: "bottom" });
+    } catch (error) {
+      console.log(error, "loginUser_error");
+      dispatch({ type: "IS_LOADER", payload: false });
+      const errorMessage = await getFirebaseErrorMessage((error as any).code);
+      Toast.show({ type: "error", text1: errorMessage, position: "bottom" });
+    }
+  };
 
 //register user
 export const registerUser =
@@ -406,7 +415,6 @@ export const addSchedule =
       };
 
       await scheduleRef.set(scheduleData);
-      // await sendEmailAPI(formData);
       // Schedule notifications for the booking
       await scheduleBookingNotifications(scheduleData);
 
@@ -420,7 +428,17 @@ export const addSchedule =
         position: "bottom",
       });
 
-      navigation.goBack();
+      dispatch(
+        sendEmail(
+          navigation,
+          formData.email,
+          formData.visitDates,
+          formData.visitTime,
+          formData.numberOfVisitors,
+          formData.numberOfVisitors,
+          formData.property
+        )
+      );
     } catch (error: any) {
       console.error("Add Schedule Error:", error);
       dispatch({ type: "IS_LOADER", payload: false });
@@ -523,7 +541,7 @@ export const updatePropertyRevenue =
       dispatch({ type: "IS_LOADER", payload: false });
     }
   };
-  
+
 // fetch notifications by user id
 export const fetchNotificationsByUserID =
   (userID: string) => async (dispatch: any) => {
