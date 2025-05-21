1e9a5decde3f5ffcbb9518f92e8f6c91c9944f86
AneelaShakeel1 <eb20103017.aneelashakeel@gmail.com>
2025-05-21 18:13:26 +0500

    app icon on notification

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,27 +1,9 @@
 const Images: {
   ProfilePlaceholder: any;
-  Banner: any;
-  GalleryImage1: any;
-  GalleryImage2: any;
-  GalleryImage3: any;
-  GalleryImage4: any;
-  GalleryImage5: any;
-  GalleryImage6: any;
-  map: any;
-  map1: any;
   Excel: any;
   Profile: any;
 } = {
   ProfilePlaceholder: require("./profilePlaceholder.png"),
-  Banner: require("./Banner.png"),
-  GalleryImage1: require("./Gallery/1.png"),
-  GalleryImage2: require("./Gallery/2.png"),
-  GalleryImage3: require("./Gallery/3.png"),
-  GalleryImage4: require("./Gallery/4.png"),
-  GalleryImage5: require("./Gallery/5.png"),
-  GalleryImage6: require("./Gallery/6.png"),
-  map: require("./map.png"),
-  map1: require("./map1.png"),
   Excel: require("./Excel.png"),
   Profile: require("./Profile.png"),
 };
--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -32,6 +32,27 @@ export const setupNotificationHandlers = () => {
         pressAction: {
           id: "default",
         },
+        smallIcon: "ic_launcher",
+        largeIcon: "ic_launcher",
+        showTimestamp: true,
+      },
+    });
+  });
+
+  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+    console.log("Received background FCM message:", remoteMessage);
+    await notifee.displayNotification({
+      title: remoteMessage.notification?.title,
+      body: remoteMessage.notification?.body,
+      android: {
+        channelId: "bookings",
+        importance: AndroidImportance.HIGH,
+        pressAction: {
+          id: "default",
+        },
+        smallIcon: "ic_launcher",
+        largeIcon: "ic_launcher",
+        showTimestamp: true,
       },
     });
   });
