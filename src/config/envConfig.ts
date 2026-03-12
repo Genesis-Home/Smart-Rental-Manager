8db8fbd77912233f88414d848574cbf3cd2bfe25
Abdullah Shah <abddullahshah@gmail.com>
2026-03-12 17:20:17 +0500

    key [update]

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -6,7 +6,8 @@ export const EnvConfig = {
   //   geocodeUrl: GOOGLE_MAPS_URL,
   // },
   googleMaps: {
-    apiKey: 'AIzaSyAN1-XDuQSu2O6V4nwbQP7M-U3xWO1ENDM',
+    // apiKey: 'AIzaSyAN1-XDuQSu2O6V4nwbQP7M-U3xWO1ENDM',
+    apiKey: 'AIzaSyCoGkoAkwhy90RWtZrFG2sQ-J5NSH5BKIM',
     geocodeUrl: "https://maps.googleapis.com/maps/api/geocode/json",
   },
 };
